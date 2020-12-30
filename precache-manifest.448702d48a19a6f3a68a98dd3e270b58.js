182d77ab8f3034e706b80aeb092acb15992beb30
ajdev <aaron.dev1124@gmail.com>
2020-12-31 02:40:56 +0800

    Updates

---
+++ precache-manifest.448702d48a19a6f3a68a98dd3e270b58.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33d905839f55893a0ff650bc1f066a1",
+    "url": "/infant.behavior/index.html"
+  },
+  {
+    "revision": "642b2b99dae041e2fed9",
+    "url": "/infant.behavior/static/css/4.89f067aa.chunk.css"
+  },
+  {
+    "revision": "7d85d9da61d61d122808",
+    "url": "/infant.behavior/static/css/main.2c1a880d.chunk.css"
+  },
+  {
+    "revision": "7571ad4210730e9af461",
+    "url": "/infant.behavior/static/js/3.dd2621b0.chunk.js"
+  },
+  {
+    "revision": "642b2b99dae041e2fed9",
+    "url": "/infant.behavior/static/js/4.70fb0877.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/infant.behavior/static/js/4.70fb0877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb354547f11d09c2218",
+    "url": "/infant.behavior/static/js/5.f580cb5e.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/infant.behavior/static/js/5.f580cb5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405b82837455c7e57aef",
+    "url": "/infant.behavior/static/js/6.46dd5e27.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/infant.behavior/static/js/6.46dd5e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d85d9da61d61d122808",
+    "url": "/infant.behavior/static/js/main.ae510d80.chunk.js"
+  },
+  {
+    "revision": "1ff01e3a381011a69f6a",
+    "url": "/infant.behavior/static/js/runtime-main.134aa2e0.js"
+  },
+  {
+    "revision": "0df70d23af12eae1155e",
+    "url": "/infant.behavior/static/js/xlsx.56c96a98.chunk.js"
+  }
+]);
