ac75a715cd1c8004467e13d94d821230b58333b8
ajdev <aaron.dev1124@gmail.com>
2020-12-31 05:39:07 +0800

    Made basic bar chart.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,104 @@
 import React, { Component } from "react";
 import "./App.css";
 import BarChart from "./BarChart";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 export class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       items: [
-        { name: "Food", value: "200", key: "1" },
-        { name: "Cloth", value: "300", key: "2" },
-        { name: "Supply", value: "100", key: "3" },
-        { name: "Water", value: "250", key: "4" },
-        { name: "Fuel", value: "150", key: "5" },
+        {
+          key: "1",
+          name: "Listening by keeping quiet, responds to pain, and touch.",
+          value: "1",
+          date: new Date(),
+        },
+        {
+          key: "2",
+          name:
+            "Communicates, seeks attention by crying or rapidly turning head sideways, and moving arms, legs and body.",
+          value: "2",
+          date: new Date(),
+        },
+        {
+          key: "3",
+          name: "Notices mother’s or caregiver’s presence, or absence.",
+          value: "3",
+          date: new Date(),
+        },
+        {
+          key: "4",
+          name:
+            "Begins studying faces, gazing at mother’s or caregiver’s face with interest. Can tell when mother or caregiver is around.",
+          value: "4",
+          date: new Date(),
+        },
+        {
+          key: "5",
+          name: "Smiles to parents or smiles back.",
+          value: "5",
+          date: new Date(),
+        },
+        {
+          key: "6",
+          name:
+            "Reaches for familiar people or objects. Waits eagerly to be fed.",
+          value: "6",
+          date: new Date(),
+        },
+        {
+          key: "7",
+          name: "Babbles, laughs, orient to voice, ad enjoy looking.",
+          value: "7",
+          date: new Date(),
+        },
+        {
+          key: "8",
+          name: "Develops a fear of strangers.",
+          value: "8",
+          date: new Date(),
+        },
+        {
+          key: "9",
+          name: "Indicates objects of interest by pointing fingers.",
+          value: "9",
+          date: new Date(),
+        },
+        {
+          key: "10",
+          name: "Waves bye-bye, plays pat-a-cake, peke-a-boo.",
+          value: "10",
+          date: new Date(),
+        },
+        {
+          key: "11",
+          name: "Develops Joint attention, actively explores environment.",
+          value: "11",
+          date: new Date(),
+        },
+        {
+          key: "12",
+          name: "Says Dada, Mama, knows his name, and comes when called.",
+          value: "12",
+          date: new Date(),
+        },
+        {
+          key: "13",
+          name:
+            "Checks with caregiver before doing certain activities (Social referencing), learns rules and cooperates with parents.",
+          value: "13",
+          date: new Date(),
+        },
+        {
+          key: "14",
+          name:
+            "Points to indicate needs, has a budding vocabulary of at least 3-6 news words other than mama and Dada.",
+          value: "14",
+          date: new Date(),
+        },
       ],
-      charts: "bar",
-      count: 0,
     };
   }
 
@@ -30,23 +114,28 @@ export class App extends Component {
             <table className="data-table">
               <tbody>
                 <tr>
-                  <th>Name</th>
-                  <th>Value</th>
-                  <th style={{ width: 80 }}>Option</th>
+                  <th>Behaviour</th>
+                  <th style={{ width: 30 }}>Date</th>
                 </tr>
                 {entry.map((item) => {
                   return (
                     <tr key={item.key}>
                       <td>{item.name}</td>
-                      <td>{item.value}</td>
                       <td>
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          onClick={() => this.deleteItem(item.key)}
-                        >
-                          Delete
-                        </button>
+                        <DatePicker
+                          selected={item.date}
+                          onChange={(date) => {
+                            let index = this.state.items.findIndex(
+                              (el) => el.key == item.key
+                            );
+                            item.date = date;
+
+                            let items = this.state.items;
+                            items[index] = item;
+
+                            this.setState(items);
+                          }}
+                        />
                       </td>
                     </tr>
                   );
@@ -59,7 +148,7 @@ export class App extends Component {
         <div className="rightContent">
           <div className="chart-container">
             <div className="chart">
-              <BarChart data={this.state.items} id={this.state.count} />
+              <BarChart data={this.state.items} id={this.state.items.length} />
             </div>
           </div>
         </div>
--- src/BarChart.js
+++ src/BarChart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
@@ -6,65 +6,64 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 am4core.useTheme(am4themes_animated);
 
 export class BarChart extends Component {
-    
-    componentDidMount() {
+  componentDidMount() {
+    const newId = this.props.id;
+    let Id = "bar-chart" + newId.toString();
 
-        const newId = this.props.id;
-        let Id = "bar-chart" + (newId.toString());
+    let chart = am4core.create(`${Id}`, am4charts.XYChart);
+    let data = this.props.data;
+    chart.data = data;
 
-        let chart = am4core.create(`${Id}`, am4charts.XYChart);
-        let data = this.props.data;
-        chart.data = data;
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "name";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
 
-        let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "name";
-        categoryAxis.renderer.grid.template.location = 0;
-        categoryAxis.renderer.minGridDistance = 30;
-
-        categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
-        if (target.dataItem && target.dataItem.index & 2 == 2) {
-            return dy + 25;
+    categoryAxis.renderer.labels.template.adapter.add(
+      "dy",
+      function (dy, target) {
+        if (target.dataItem && target.dataItem.index & (2 == 2)) {
+          return dy + 25;
         }
         return dy;
-        });
+      }
+    );
 
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-        // Create series
-        let series = chart.series.push(new am4charts.ColumnSeries());
-        series.dataFields.valueY = "value";
-        series.dataFields.categoryX = "name";
-        series.name = "Value";
-        series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
-        series.columns.template.fillOpacity = .8;
+    // Create series
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.categoryX = "name";
+    series.name = "Value";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = 0.8;
 
-        let columnTemplate = series.columns.template;
-        columnTemplate.strokeWidth = 2;
-        columnTemplate.strokeOpacity = 1;
-        this.chart = chart;
+    let columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
+    this.chart = chart;
+  }
 
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
     }
-    
-    componentWillUnmount() {
-        if (this.chart) {
-            this.chart.dispose();
-        }
-    }
+  }
 
-    render() {
-        
-        const newId = this.props.id;
-        let Id = "bar-chart" + (newId.toString());
+  render() {
+    const newId = this.props.id;
+    let Id = "bar-chart" + newId.toString();
 
-        return (
-            <div className="barchart">
-                <div className="title" style={{width: "100%", textAlign: "center"}}>
-                    <h3 style={{padding: 10}}>Bar Chart</h3>
-                </div>
-                <div id={`${Id}`} style={{ width: "100%", height: 400}} ></div>
-            </div>
-        )
-    }
+    return (
+      <div className="barchart">
+        <div className="title" style={{ width: "100%", textAlign: "center" }}>
+          <h3 style={{ padding: 10 }}>Bar Chart</h3>
+        </div>
+        <div id={`${Id}`} style={{ width: "100%", height: 400 }}></div>
+      </div>
+    );
+  }
 }
 
-export default BarChart
+export default BarChart;
